911989077623e6684d695ad767e3d45fbdd06c6c
Mari Jaoshvili <marijaoshvili@Maris-MacBook-Air.local>
2022-11-30 13:50:54 -0500

    completed most of functions

--- discussion_12.py
+++ discussion_12.py
@@ -16,7 +16,8 @@ def setUpDatabase(db_name):
 # TASK 1
 # CREATE TABLE FOR EMPLOYEE INFORMATION IN DATABASE AND ADD INFORMATION
 def create_employee_table(cur, conn):
-    pass
+    cur.execute('create table if not exists Employees (employee_id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT, job_id INTEGER, hire_date DATE, salary NUMBER)')
+    conn.commit()
 
 # ADD EMPLOYEE'S INFORMTION TO THE TABLE
 
@@ -27,16 +28,37 @@ def add_employee(filename, cur, conn):
     file_data = f.read()
     f.close()
     # THE REST IS UP TO YOU
-    pass
+    data = json.loads(file_data)
+    for item in data:
+        emp_id = item['employee_id']
+        f_name = item['first_name']
+        l_name = item['last_name']
+        hire_date = item['hire_date']
+        job_id = item['job_id']
+        salary = item['salary']
+
+        cur.execute('INSERT or ignore INTO Employees (employee_id, first_name, last_name, job_id, hire_date, salary) values(?,?,?,?,?,?)', (emp_id, f_name, l_name, job_id, hire_date, salary))
+        conn.commit()
 
 # TASK 2: GET JOB AND HIRE_DATE INFORMATION
 def job_and_hire_date(cur, conn):
-    pass
+    cur.execute('SELECT job_id, hire_date FROM Employees')
+    data = cur.fetchall()
+    data = sorted(data)
+    job_id = data[0][1]
+    cur.execute('SELECT job_title from jobs where job_id = ?', (job_id,))
+    job_title = cur.fetchall()
+    return job_title[0][0]
+
+    
 
 # TASK 3: IDENTIFY PROBLEMATIC SALARY DATA
 # Apply JOIN clause to match individual employees
 def problematic_salary(cur, conn):
-    pass
+    cur.executre('select Employees.first_name, Employees.last_name join jobs.job_id = employees.job_id where Employees.salary < jobs.min_salary or Employees.salary > jobs.max_salary')
+    x = cur.fetchall()
+    return x 
+
 
 # TASK 4: VISUALIZATION
 def visualization_salary_data(cur, conn):
